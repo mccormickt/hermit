fe226ff8339e369fda67c5f7e246ffbb6ff84e82
jan0ski <mccormickt9@gmail.com>
2023-01-08 23:15:08 -0500

    wip: certificate errors on kube client

--- hermit-webhook/src/main.rs
+++ hermit-webhook/src/main.rs
@@ -3,14 +3,19 @@ use actix_web::{
 };
 use anyhow::Result;
 use env_logger::Env;
-use kube::core::{
-    admission::{AdmissionRequest, AdmissionResponse, AdmissionReview},
-    DynamicObject,
+use kube::{
+    core::{
+        admission::{AdmissionRequest, AdmissionResponse, AdmissionReview},
+        DynamicObject,
+    },
+    ResourceExt,
 };
 use serde_json::json;
-use tracing::{debug, error, info, warn};
+use tracing::{error, info, warn};
 
-#[get("/health")]
+mod util;
+
+#[get("/healthz")]
 async fn health() -> impl Responder {
     HttpResponse::Ok()
         .header(http::header::CONTENT_TYPE, "application/json")
@@ -28,14 +33,6 @@ async fn handle_mutate(
         reqst.uri(),
     );
 
-    if let Some(content_type) = reqst.head().headers.get("content-type") {
-        if content_type != "application/json" {
-            let msg = format!("invalid content-type: {:?}", content_type);
-            warn!("Warn: {}, Code: {}", msg, http::StatusCode::BAD_REQUEST);
-            return HttpResponse::BadRequest().json(msg);
-        }
-    }
-
     let req: AdmissionRequest<_> = match body.into_inner().try_into() {
         Ok(req) => req,
         Err(err) => {
@@ -45,15 +42,29 @@ async fn handle_mutate(
         }
     };
 
-    let resp = AdmissionResponse::from(&req);
+    let mut res = AdmissionResponse::from(&req);
 
-    HttpResponse::Ok().json(&resp.into_review())
+    if let Some(obj) = req.object {
+        let name = obj.name_any();
+        info!("injecting envoy sidecar in pod {}", name);
+        res = match util::inject_sidecar(res.clone(), &obj).await {
+            Ok(res) => {
+                info!("accepted: {:?} on Pod {}", req.operation, name);
+                res
+            }
+            Err(err) => {
+                warn!("denied: {:?} on {} ({})", req.operation, name, err);
+                res.deny(err.to_string())
+            }
+        };
+    };
+
+    HttpResponse::Ok().json(&res.into_review())
 }
 
-#[actix_web::main]
+#[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     info!("Started http server: 0.0.0.0:8080");
-
     //let mut config = ServerConfig::new(NoClientAuth::new());
     //let cert_file = &mut BufReader::new(File::open("./certs/serverCert.pem")?);
     //let key_file = &mut BufReader::new(File::open("./certs/serverKey.pem")?);
@@ -61,7 +72,7 @@ async fn main() -> Result<(), anyhow::Error> {
     //let mut keys = rsa_private_keys(key_file).expect("error in key");
     //config.set_single_cert(cert_chain, keys.remove(0))?;
 
-    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    env_logger::init_from_env(Env::default().default_filter_or("debug"));
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
@@ -69,7 +80,7 @@ async fn main() -> Result<(), anyhow::Error> {
             .service(handle_mutate)
     })
     //.bind_rustls("0.0.0.0:8443")?
-    .bind("0.0.0.0:8443")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await?;
 
